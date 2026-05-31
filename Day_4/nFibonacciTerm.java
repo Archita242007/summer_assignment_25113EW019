7da097e1d56d002f1688c0c2cb63f871b2fd3130
Archita242007 <architasablok2652@gmail.com>
2026-05-31 23:32:41 +0530

    Added Day 4 problems

---
+++ Day_4/nFibonacciTerm.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class nFibonacciTerm {
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+     System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        if (n == 1) {
+            System.out.println(a);
+        } else if (n == 2) {
+            System.out.println(b);
+        } else {
+            for (int i = 3; i <= n; i++) {
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+            System.out.println("Nth Fibonacci Term = " + b);
+        }
+    sc.close();
+}
+}
