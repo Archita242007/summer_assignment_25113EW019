2729296dd73a9307edefe09234b7d563bfe948fe
Archita242007 <architasablok2652@gmail.com>
2026-06-11 07:46:04 +0530

    Added Day 13 problems

---
+++ Day_13/evenOddCount.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class evenOddCount {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        int even = 0;
+        int odd = 0;
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if(arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Even Count = " + even);
+        System.out.println("Odd Count = " + odd);
+
+        sc.close();
+    }
+}
