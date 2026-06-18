43ae86156fd4560fa2777b57a73e4a3bd35374f7
Archita242007 <architasablok2652@gmail.com>
2026-06-18 08:08:43 +0530

    Added Day 19 problems

---
+++ Day_19/transpose.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class transpose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] matrix = new int[r][c];
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Transpose:");
+
+        for(int i = 0; i < c; i++) {
+            for(int j = 0; j < r; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
